7a822201448e07c20c6058550107c30c8d37e4ae
Joao Lucas <jlmelosouza18@gmail.com>
2021-03-15 21:05:01 -0300

    refactoring

--- src/modules/dashboard/services/CreateProductService.ts
+++ src/modules/dashboard/services/CreateProductService.ts
@@ -29,13 +29,22 @@ class CreateProductService {
       throw new AppError('Product already created.', 404);
     }
 
-    const titleLower = title.toLocaleLowerCase();
+    const accentsMap = {
+      a: 'á|à|ã|â|À|Á|Ã|Â',
+      e: 'é|è|ê|É|È|Ê',
+      i: 'í|ì|î|Í|Ì|Î',
+      o: 'ó|ò|ô|õ|Ó|Ò|Ô|Õ',
+      u: 'ú|ù|û|ü|Ú|Ù|Û|Ü',
+      c: 'ç|Ç',
+      n: 'ñ|Ñ',
+    };
 
-    const slug = titleLower
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '')
-      .split(' ')
-      .join('-');
+    const titleFormatted = Object.keys(accentsMap).reduce(
+      (acc, cur) => acc.replace(new RegExp(accentsMap[cur], 'g'), cur),
+      title.toLocaleLowerCase(),
+    );
+
+    const slug = titleFormatted.split(' ').join('-');
 
     const product = await Product.create({
       _id: new ObjectID(),
